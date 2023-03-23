2beedcbbe921e2cfc9c08186d5a961d708d2feb5
askoryshka <112905257+askoryshka@users.noreply.github.com>
2023-03-23 15:57:34 +0300

    Delete 2 семестр/1 directory

--- 2 семестр/1/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Matrix matrix = new Matrix(3);
-        matrix.get();
-        System.out.println("Транспонированная матрица: \n" + matrix.transposition());
-    }
-}
