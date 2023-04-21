35f8ee5f427c30ed0a52d522052ae5b5b80a1f18
askoryshka <112905257+askoryshka@users.noreply.github.com>
2023-04-21 11:00:29 +0300

    Delete задание 5 directory

--- задание 5/3.py
+++
@@ -1,9 +0,0 @@
-def x(n):
-    a=0
-    b=1
-    while b<=n:
-        b*=2
-        a+=1
-        return a-1, b//2
-f=int(input('Введите число:'))
-print(x(f))
--- задание 5/4.py
+++
@@ -1,9 +0,0 @@
-def x(x,y):
-    den=1
-    while y>x:
-        x *= 1.1
-        den += 1
-    print(den)
-a=int(input('Расстояние 1:'))
-b=int(input('Расстояние 2:'))
-print(x(a,b))
