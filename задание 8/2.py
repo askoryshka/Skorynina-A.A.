8f649691483e9a9c0421a0864ad000fb32118919
askoryshka <112905257+askoryshka@users.noreply.github.com>
2023-04-21 11:01:04 +0300

    Delete задание 8 directory

--- задание 8/2.py
+++
@@ -1,9 +0,0 @@
-def massive(m, a):
-    a[0], a[m - 1] = a[m - 1], a[0]
-    print(a)
-
-
-M = int(input('Введите кол-во элементов массива:'))
-j = []
-j = [int(input("Введите элемент массива:")) for i in range(M)]
-print(massive(M, j))
