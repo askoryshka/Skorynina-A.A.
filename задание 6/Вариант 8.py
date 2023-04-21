2f6f02f0d985e9081a1f6977501f229e316380c4
askoryshka <112905257+askoryshka@users.noreply.github.com>
2023-04-21 11:00:39 +0300

    Delete задание 6 directory

--- задание 6/Вариант 8.py
+++
@@ -1,8 +0,0 @@
-def x(t):
-    a=0
-    for i in range (len(t)):
-        if (t[i] == '.') or (t[i] == ' '):
-            a+=1
-    print('Кол-во слов:', a)
-text=str(input('Введите предложение:'))
-print(x(text))
