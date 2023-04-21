a96416291bede225170d1f2059a1f0664c0cd8f7
askoryshka <112905257+askoryshka@users.noreply.github.com>
2023-04-21 10:56:51 +0300

    Delete задание 3 directory

--- задание 3/2.py
+++
@@ -1,3 +0,0 @@
-kat1=int(input())
-kat2=int(input())
-print(1/2*kat1*kat2)
--- задание 3/3.py
+++
@@ -1,7 +0,0 @@
-def time(n):
-    n=n%60
-    chas=n//60%24
-    return chas, n
-minuta=int(input())
-chas,minuta=time(minuta)
-print(chas,":",minuta)
